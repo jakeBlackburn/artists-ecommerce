9ddb4bab69b113710176e7ee0da9218c1389605d
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2022-10-04 09:07:32 -0400

    fixing patch route in router

--- server/router.js
+++ server/router.js
@@ -10,7 +10,7 @@ router.route('/login').post(login)
 router.route('/artists').get(getAllArtists)
 router.route('/artworks').get(getAllArtworks)
 router.route('/artists/:name').get(getArtist)
-router.route('/artworks/:name').get(getArtwork).post(updateArtworkAuction)
+router.route('/artworks/:name').get(getArtwork).patch(updateArtworkAuction)
 router.route('/artists/profile/:name').get(getArtistAndArtworks)
 
 
