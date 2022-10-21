0edddb3e73dc2fcc8b0c4902d61c86fd80dbae33
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2022-10-21 11:45:05 -0400

    adding user data & population

--- server/populate.js
+++ server/populate.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 
 const connectDB = require('./db')
-const { Artwork, Artist } = require('./models')
+const { Artwork, Artist, User } = require('./models')
 
 const Artworks = require('./data/artworks.json')
 const Artists = require('./data/artists.json')
+const Users = require('./data/users.json')
 
 const username = process.env.ATLAS_USERNAME
 const password = process.env.ATLAS_PASSWORD
@@ -12,10 +13,15 @@ const password = process.env.ATLAS_PASSWORD
 const start = async () => {
   try {
     await connectDB(`mongodb+srv://${username}:${password}@portfolio-cluster.sknurkv.mongodb.net/e-commerce?retryWrites=true&w=majority`);
+
     await Artwork.deleteMany()
     await Artist.deleteMany()
+    await User.deleteMany()
+
     await Artwork.create(Artworks)
     await Artist.create(Artists)
+    await User.create(Users)
+
     console.log('Success!!!!')
     process.exit(0)
   } catch (error) {
